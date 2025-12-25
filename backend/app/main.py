169d54f2343f7f193924c865519d9157cd4b3eab
Nihal Shah <nihalshah4@gmail.com>
2025-12-24 21:52:38 -0500

    Updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.api.upload import router as upload_router
 from app.api.datasets import router as datasets_router
 from app.api.profile import router as profile_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload_router)
 app.include_router(datasets_router)
